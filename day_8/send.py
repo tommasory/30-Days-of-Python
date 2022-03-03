b0bc4961a7f84e7e93935bb5cc37857b3735a337
Tommasory <tommasory@gmail.com>
2022-03-03 17:53:19 -0500

    Day 8 - Importing - Internal & External.

---
+++ day_8/send.py
@@ -0,0 +1,30 @@
+import sys
+import requests
+from datetime import datetime
+
+from formatting import format_msg
+
+def send(name, website=None, verbose=False):
+    if website != None:
+        msg = format_msg(my_name=name, my_website=website)
+    else:
+        msg = format_msg(my_name=name)
+    print(msg)
+    if verbose:
+        print('- '*10)
+        print(name, website)
+        print('- '*10)
+    # send the message
+    r = requests.get("http://httpbin.org/json")
+    if r.status_code == 200:
+        return r.json()
+    else:
+        return "There was an error"
+
+if __name__ == "__main__":
+    print(sys.argv)
+    name = "Unknown"
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+    response = send(name, verbose=True)
+    print(response)
